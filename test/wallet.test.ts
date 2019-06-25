bb682ac454b168872d2b36fffedac09849d411ec
adcpm <fabien@bonustrack.co>
2019-06-26 06:07:33 +0700

    Add wallet test

---
+++ test/wallet.test.ts
@@ -0,0 +1,16 @@
+import { Mnemonic } from '../lib/wallet/Mnemonic';
+
+describe('LibraWallet', () => {
+  describe('generateMnemonic()', () => {
+    it('should generate 100 unique mnemonic', () => {
+      const mnemonics = [];
+
+      for (let i = 0; i < 100; i++) {
+        const mnemonic = new Mnemonic().toString();
+        mnemonics.push(mnemonic);
+      }
+
+      expect([...new Set(mnemonics)].length).toBe(100);
+    });
+  });
+});
